d0ca467d6c7f72f90799c2853d8c84ebb499b371
Alisson <alissonmartin2011@hotmail.com>
2025-07-19 18:26:15 -0300

    Feat: Adding functions

--- tool/GenerateAst.java
+++ tool/GenerateAst.java
@@ -12,9 +12,9 @@ public class GenerateAst {
              System.exit(64);
          }
          String outputDir = args[0];
-         defineAst(outputDir, "Expr", Arrays.asList("Assign : Token name, Expr value", "Binary : Expr left, Token operator, Expr right", "Grouping : Expr expression", "Literal : Object value", "Logical : Expr left, Token operator, Expr right", "Unary : Token operator, Expr right", "Ternary : Expr condition, Expr trueCondition, Expr falseCondition", "Variable : Token name"));
+         defineAst(outputDir, "Expr", Arrays.asList("Assign : Token name, Expr value", "Binary : Expr left, Token operator, Expr right", "Call : Expr callee, Token paren, List<Expr> arguments", "Grouping : Expr expression", "Literal : Object value", "Logical : Expr left, Token operator, Expr right", "Unary : Token operator, Expr right", "Ternary : Expr condition, Expr trueCondition, Expr falseCondition", "Variable : Token name"));
 
-         defineAst(outputDir, "Stmt", Arrays.asList("Block : List<Stmt> statements","Expression : Expr expression", "IfCondition : Expr condition, Stmt thenBranch," + " Stmt elseBranch", "Print : Expr expression", "Var : Token name, Expr initializer", "WhileLoop : Expr condition, Stmt body"));
+         defineAst(outputDir, "Stmt", Arrays.asList("Block : List<Stmt> statements","Expression : Expr expression", "Function : Token name, List<Token> params, List<Stmt> body", "IfCondition : Expr condition, Stmt thenBranch," + " Stmt elseBranch", "Print : Expr expression", "Var : Token name, Expr initializer", "WhileLoop : Expr condition, Stmt body"));
     }
 
 
